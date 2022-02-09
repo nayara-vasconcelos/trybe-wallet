1cfc6496d3ebd099f500e8e303ac6dcb2de18643
Nayara Vasconcelos <nayaravasconcelos.dev@gmail.com>
2022-02-09 18:23:41 -0300

    feat: configura o redux

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,14 @@
 // Coloque aqui suas actions
+
+export const SAVE_USER_EMAIL = 'SAVE_EMAIL';
+export const SAVE_WALLET_EXPENSES = 'SAVE_WALLET_EXPENSES';
+
+export const saveEmail = (payload) => ({
+  type: SAVE_USER_EMAIL,
+  payload,
+});
+
+export const saveExpenses = (payload) => ({
+  type: SAVE_WALLET_EXPENSES,
+  payload,
+});
--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,1 +1,22 @@
 // Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+
+import { SAVE_WALLET_EXPENSES } from '../actions';
+
+const INITIAL_STATE = {
+  currencies: [],
+  expenses: [],
+};
+
+const walletReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+  case SAVE_WALLET_EXPENSES:
+    return ({
+      ...state,
+      expenses: [...state.expenses, action.payload],
+    });
+  default:
+    return state;
+  }
+};
+
+export default walletReducer;
