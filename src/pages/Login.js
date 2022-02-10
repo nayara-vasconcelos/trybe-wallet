54d9c928703a7d7e3799fe2f7b93d8d34fa54406
Nayara Vasconcelos <nayaravasconcelos.dev@gmail.com>
2022-02-09 21:04:37 -0300

    feat: requisito 1 - adiciona formulario de login controlado

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,63 @@
 import React from 'react';
 
+const INITIAL_STATE = {
+  email: '',
+  password: '',
+  isBtnDisabled: true,
+};
+
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = INITIAL_STATE;
+  }
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+
+    this.setState({ [name]: value });
+  }
+
   render() {
-    return <div>Login</div>;
+    const { email, password, isBtnDisabled } = this.state;
+    return (
+      <div>
+        <h2>Login</h2>
+        <form>
+          {/* Campo para e-mail */}
+          <label htmlFor="email-input">
+            e-mail:
+            <input
+              type="e-mail"
+              id="email-input"
+              name="email"
+              value={ email }
+              data-testid="email-input"
+              onChange={ this.handleChange }
+              placeholder="Digite seu email"
+              autoComplete="off"
+            />
+          </label>
+          {/* Campo para senha */}
+          <label htmlFor="password-input">
+            Senha:
+            <input
+              type="password"
+              id="password-input"
+              name="password"
+              value={ password }
+              data-testid="password-input"
+              onChange={ this.handleChange }
+              placeholder="Digite sua senha"
+            />
+          </label>
+          <button type="submit" disabled={ isBtnDisabled }>
+            Entrar
+          </button>
+        </form>
+      </div>
+    );
   }
 }
 
