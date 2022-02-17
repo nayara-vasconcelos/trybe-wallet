e72d9f3f56f0854a7cd8cb44dfc53a37a8f868fd
Nayara Vasconcelos <nayaravasconcelos.dev@gmail.com>
2022-02-17 19:58:43 -0300

    feat: requisito 5
    Faz requisicao a API assim que o componente é montado e renderiza
    na tela as opcoes das moedas recebidas por props da store

--- src/components/ExpensesForm.js
+++ src/components/ExpensesForm.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import { addExpense } from '../actions';
+import { addExpense, fetchCurrencies } from '../actions';
 
 /**
  * Formato no Redux:
@@ -39,9 +39,13 @@ class ExpensesForm extends Component {
     this.state = INITIAL_STATE;
   }
 
+  componentDidMount() {
+    const { requestCurrencies } = this.props;
+    requestCurrencies();
+  }
+
   handleChange = ({ target }) => {
     const { name, value } = target;
-
     this.setState({ [name]: value });
   };
 
@@ -58,74 +62,101 @@ class ExpensesForm extends Component {
     this.setState(INITIAL_STATE);
   }
 
+  renderCurrencySelect = () => {
+    const { currency } = this.state;
+    const { currencies } = this.props;
+
+    const currencyOptions = currencies.map((option) => (
+      <option
+        key={ option }
+        value={ option }
+        data-testid={ option }
+      >
+        { option }
+      </option>
+    ));
+
+    return (
+      <label htmlFor="moeda">
+        Moeda:
+        <select
+          name="currency"
+          id="moeda"
+          value={ currency }
+          data-testid="currency-input"
+          onChange={ this.handleChange }
+        >
+          { currencyOptions }
+        </select>
+      </label>
+    );
+  }
+
   render() {
-    const { value, description, currency, method, tag } = this.state;
+    const { value, description, method, tag } = this.state;
+    const { currencies } = this.props;
 
     return (
       <section>
         <form>
           {/* Campo para valor da despesa */}
-          <input
-            type="number"
-            data-testid="value-input"
-            name="value"
-            value={ value }
-            onChange={ this.handleChange }
-          />
+          <label htmlFor="despesa">
+            Despesa:
+            <input
+              type="number"
+              id="despesa"
+              data-testid="value-input"
+              name="value"
+              value={ value }
+              onChange={ this.handleChange }
+            />
+          </label>
           {/* Campo para descrição da despesa */}
-          <input
-            type="text"
-            data-testid="description-input"
-            name="description"
-            value={ description }
-            onChange={ this.handleChange }
-          />
+          <label htmlFor="descricao">
+            Descrição:
+            <input
+              type="text"
+              id="descricao"
+              data-testid="description-input"
+              name="description"
+              value={ description }
+              onChange={ this.handleChange }
+            />
+          </label>
           {/* Campo para escolher moeda */}
-          <select
-            name="currency"
-            value={ currency }
-            data-testid="currency-input"
-            onChange={ this.handleChange }
-          >
-            <option value="USD">USD</option>
-            <option value="CAD">CAD</option>
-            <option value="EUR">EUR</option>
-            <option value="GBP">GBP</option>
-            <option value="ARS">ARS</option>
-            <option value="BTC">BTC</option>
-            <option value="LTC">LTC</option>
-            <option value="JPY">JPY</option>
-            <option value="CHF">CHF</option>
-            <option value="AUD">AUD</option>
-            <option value="CNY">CNY</option>
-            <option value="ILS">ILS</option>
-            <option value="ETH">ETH</option>
-            <option value="XRP">XRP</option>
-          </select>
+          { (currencies.length > 0) && this.renderCurrencySelect() }
           {/* Campo para escolher método de pagamento */}
-          <select
-            name="method"
-            value={ method }
-            data-testid="method-input"
-            onChange={ this.handleChange }
-          >
-            <option value="Dinheiro'">Dinheiro</option>
-            <option value="Cartão de crédito">Cartão de crédito</option>
-            <option value="Cartão de débito">Cartão de débito</option>
-          </select>
+          <label htmlFor="metodo">
+            Método de pagamento:
+            <select
+              name="method"
+              id="metodo"
+              value={ method }
+              data-testid="method-input"
+              onChange={ this.handleChange }
+            >
+              <option value="Dinheiro'">Dinheiro</option>
+              <option value="Cartão de crédito">Cartão de crédito</option>
+              <option value="Cartão de débito">Cartão de débito</option>
+            </select>
+          </label>
           {/* Campo para escolher a categoria da despesa */}
-          <select
-            name="tag"
-            value={ tag }
-            data-testid="tag-input"
-            onChange={ this.handleChange }
-          >
-            <option value="Alimentação">Alimentação</option>
-            <option value="Lazer">Lazer</option>
-            <option value="Trabalho">Trabalho</option>
-            <option value="Transporte">Transporte</option>
-            <option value="Saúde">Saúde</option>
-          </select>
+          <label htmlFor="categoria">
+            Categoria:
+            <select
+              name="tag"
+              id="categoria"
+              value={ tag }
+              data-testid="tag-input"
+              onChange={ this.handleChange }
+            >
+              <option value="Alimentação">Alimentação</option>
+              <option value="Lazer">Lazer</option>
+              <option value="Trabalho">Trabalho</option>
+              <option value="Transporte">Transporte</option>
+              <option value="Saúde">Saúde</option>
+            </select>
+          </label>
           {/* Botão para salvar despesas */}
           <button
             type="button"
@@ -146,18 +177,23 @@ ExpensesForm.propTypes = {
     PropTypes.number,
     PropTypes.object,
   ]))),
+  currencies: PropTypes.arrayOf(PropTypes.string),
+  requestCurrencies: PropTypes.func.isRequired,
 };
 
 ExpensesForm.defaultProps = {
   expenses: [],
+  currencies: [],
 };
 
 const mapStateToProps = (state) => ({
   expenses: state.wallet.expenses,
+  currencies: state.wallet.currencies,
 });
 
 const mapDispatchToProps = (dispatch) => ({
   saveExpense: (expense) => dispatch(addExpense(expense)),
+  requestCurrencies: () => dispatch(fetchCurrencies()),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(ExpensesForm);
