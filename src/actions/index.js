198217abf41b3230ff4c58dff92a6d8991d522df
Nayara Vasconcelos <nayaravasconcelos.dev@gmail.com>
2022-02-17 17:38:10 -0300

    feat: requisito 4
    Adiciona action para fazer requisicao a API

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,14 +1,33 @@
 // Coloque aqui suas actions
 
 export const SAVE_USER_EMAIL = 'SAVE_EMAIL';
-export const SAVE_WALLET_EXPENSES = 'SAVE_WALLET_EXPENSES';
+export const UPDATE_EXPENSES = 'UPDATE_EXPENSES';
+export const REQUEST_EXCHANGE_RATE = 'REQUEST_EXCHANGE_RATE';
+export const FAILED_REQUEST = 'FAILED_REQUEST';
 
 export const saveEmail = (payload) => ({
   type: SAVE_USER_EMAIL,
   payload,
 });
 
-export const saveExpenses = (payload) => ({
-  type: SAVE_WALLET_EXPENSES,
+const requestAPI = () => ({
+  type: REQUEST_EXCHANGE_RATE,
+});
+
+const failedRequest = (error) => ({
+  type: FAILED_REQUEST,
+  error,
+});
+
+const updateExpenses = (payload) => ({
+  type: UPDATE_EXPENSES,
   payload,
 });
+
+export const addExpense = (payload) => (dispatch) => { // thunk declarado
+  dispatch(requestAPI());
+  return fetch('https://economia.awesomeapi.com.br/json/all')
+    .then((response) => response.json())
+    .then((exchangeRates) => dispatch(updateExpenses({ ...payload, exchangeRates })))
+    .catch((error) => dispatch(failedRequest(error)));
+};
